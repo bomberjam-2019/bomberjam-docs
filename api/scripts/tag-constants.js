d884d6c2500281c2713ec2e2ddb700aee0e31962
Joseph Passineau <joseph.passineau@gsoft.com>
2019-10-29 16:01:01 -0400

    Push fix that prevents generation to fail if bomberjam-backend constants.js is missing

--- api/scripts/tag-constants.js
+++ api/scripts/tag-constants.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 
 hexo.extend.tag.register('constants', function (args) {
-    var constants = fs.readFileSync(require.resolve('bomberjam-backend/dist/constants.js'));
+    
+    var constants = Object.assign(exports, {});
+    
+    try {
+        constants = fs.readFileSync(require.resolve('bomberjam-backend/dist/constants.js'));
+    }
+    catch(e) {}
+    
     return hexo.render.renderSync({text: '```javascript-content\n'+constants+'\n```', engine: 'markdown'});;
 }, {async: false});
